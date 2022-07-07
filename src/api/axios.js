51ff339036ab30b5da9a8f033789aba840064517
zarateganso10 <matheuszarate55@gmail.com>
2022-07-07 09:38:48 -0300

    removing env variables

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,11 +1,8 @@
 import axios from "axios";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const instanceAxios = axios.create({
-  baseURL: process.env.API_URL,
-  timeout: 1000,
+  baseURL: "https://api-clients-qualicorp.herokuapp.com/",
+  timeout: 5000,
 });
 
 export { instanceAxios };
